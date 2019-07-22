d3b0feb84f71b3050bc70312574e5107d04ba2e1
kwanghyunMoon <mgh3326@naver.com>
2019-07-22 17:15:27 +0900

    update chap 7

--- src/main/java/me/khmoon/demospringdata/CommentRepository.java
+++ src/main/java/me/khmoon/demospringdata/CommentRepository.java
@@ -1,12 +1,18 @@
 package me.khmoon.demospringdata;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.RepositoryDefinition;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.util.concurrent.ListenableFuture;
 
 import java.util.List;
+import java.util.concurrent.Future;
+import java.util.stream.Stream;
 
 public interface CommentRepository extends MyRepository<Comment, Long> {
 
-  @Query(value = "select c from Comment As c", nativeQuery = true)
-  List<Comment> findByTitleContains(String keyword);
+  @Async
+  ListenableFuture<List<Comment>> findByCommentContainsIgnoreCase(String keyword, Pageable pageable);
 }
