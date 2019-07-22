1530908eb8437aba32716e086698cdb8674b4021
kwanghyunMoon <mgh3326@naver.com>
2019-07-22 14:20:12 +0900

    update

--- src/main/java/me/khmoon/demospringdata/DemospringdataApplication.java
+++ src/main/java/me/khmoon/demospringdata/DemospringdataApplication.java
@@ -2,8 +2,11 @@ package me.khmoon.demospringdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.QueryLookupStrategy;
 
 @SpringBootApplication
+@EnableJpaRepositories(queryLookupStrategy = QueryLookupStrategy.Key.CREATE_IF_NOT_FOUND)
 public class DemospringdataApplication {
 
   public static void main(String[] args) {
---
+++ src/test/java/me/khmoon/demospringdata/CommentRepositoryTest.java
@@ -0,0 +1,40 @@
+package me.khmoon.demospringdata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class CommentRepositoryTest {
+
+  @Autowired
+  CommentRepository commentRepository;
+
+  @Test
+  public void crud() {
+//    Comment comment = new Comment();
+//    comment.setComment("Hello Comment");
+//    commentRepository.save(comment);
+//
+//    List<Comment> all = commentRepository.findAll();
+//    assertThat(all.size()).isEqualTo(1);
+//
+//    long count = commentRepository.count();
+//    assertThat(count).isEqualTo(1);
+
+    Optional<Comment> byId = commentRepository.findById(100l);
+    assertThat(byId).isEmpty();
+    Comment comment = byId.orElseThrow(IllegalAccessError::new);
+
+  }
+
+}
